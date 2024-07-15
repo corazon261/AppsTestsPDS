e327bf60d0fde0be39cd1ef04080683132b796eb
Lucas Weber <131485876+corazon261@users.noreply.github.com>
2024-07-15 10:35:19 -0300

    Create app portifólio

--- ProjectAppLucas1507/app/src/main/java/com/example/projectapplucas1507/ProjectActivity.kt
+++ ProjectAppLucas1507/app/src/main/java/com/example/projectapplucas1507/ProjectActivity.kt
@@ -1,32 +1,20 @@
 package com.example.projectapplucas1507
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-
-class MainActivity : AppCompatActivity() {
+class ProjectActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_project)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
-    fun certf(view: View){
-        val certificateActivity = Intent( this, CertificateActivity::class.java)
-        startActivity(certificateActivity)
-    }
-    fun project(view:View){
-        val projectAc = Intent(this, ProjectActivity::class.java)
-        startActivity(projectAc)
-    }
-
 }
