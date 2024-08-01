19f29d907e5bc20f5b7ee3bab5d2b56a4eac7ed2
Lucas Weber <131485876+corazon261@users.noreply.github.com>
2024-08-01 08:21:02 -0300

    added activity home

--- DinGomProject/app/src/main/java/com/example/dingomproject/HomeActivity.kt
+++ DinGomProject/app/src/main/java/com/example/dingomproject/HomeActivity.kt
@@ -1,27 +1,20 @@
 package com.example.dingomproject
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
-    fun View.RedirectToHome(){
-        val homeActivity= Intent(this@MainActivity, HomeActivity::class.java)
-        startActivity(homeActivity)
-
     }
 }
